2751fa624ac1ad99c1d0af891612c82451de4a16
Marius Breivik <marius.breivik@gmail.com>
2018-08-12 19:07:57 +0200

    authentication works now

--- main.go
+++ main.go
@@ -9,7 +9,16 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func authenticate(client *resty.Client) {
+// AuthToken comment
+type AuthToken struct {
+	AccessToken  string   `json:"access_token"`
+	RefreshToken string   `json:"refresh_token"`
+	Scope        []string `json:"scope"`
+	ExpiresIn    int      `json:"expires_in"`
+	ExpireIn     int      `json:"expire_in"`
+}
+
+func authenticate(client *resty.Client) string {
 	client.SetFormData(map[string]string{
 		"grant_type":    "password",
 		"client_id":     os.Getenv("CLIENT_ID"),
@@ -17,9 +26,14 @@ func authenticate(client *resty.Client) {
 		"scope":         "read_station",
 		"username":      os.Getenv("NETATMO_USERNAME"),
 		"password":      os.Getenv("NETATMO_PASSWD")})
-	response, error := client.R().Post("/oauth2/token")
-	fmt.Println(error)
-	fmt.Println(response)
+
+	response, error := client.R().SetResult(AuthToken{}).Post("/oauth2/token")
+
+	if error != nil {
+		panic(error)
+	}
+
+	return response.Result().(*AuthToken).AccessToken
 }
 
 func stationData(client *resty.Client, token string) {
@@ -40,5 +54,5 @@ func main() {
 	client.SetHeader("Content-Type", "application/x-www-form-urlencoded")
 	client.SetHostURL("https://api.netatmo.com")
 
-	authenticate(client)
+	fmt.Println(authenticate(client))
 }
